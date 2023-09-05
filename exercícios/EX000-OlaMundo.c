79cda89d350fb131d5b9745ce7eeb81d5ae0598c
Vanderlei Santana <109047525+VanderleiSantanaTI@users.noreply.github.com>
2023-09-05 17:37:16 -0300

    1.1

--- exercícios/EX000-OlaMundo.c
+++ exercícios/EX000-OlaMundo.c
@@ -2,6 +2,6 @@
 #include <locale.h>
 void main(){
     setlocale(LC_ALL, "portuguese");
-    printf("\nOlá, Mundo!\n");
+    printf("\nOlá, Mundo!!!\n");
     printf("Ufa! Já estou \"LIVRE\" da maldição.");
 }
